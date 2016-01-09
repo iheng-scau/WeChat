15d5f1c44a69db38d920f9361e5414e9ec841e80
iheng <iheng_scau@hotmail.com>
2016-01-09 14:42:16 +0800

    add message service class

---
+++ src/org/iheng/wechat/services/MessageService.java
@@ -0,0 +1,15 @@
+package org.iheng.wechat.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MessageService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
