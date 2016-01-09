64f124e207037361c1b86a6c5ee9b5dfa5270ae5
iheng <iheng_scau@hotmail.com>
2016-01-09 11:45:05 +0800

    update

--- src/org/iheng/wechat/entities/WeChatMessage.java
+++ src/org/iheng/wechat/entities/WeChatMessage.java
@@ -1,42 +1,49 @@
 package org.iheng.wechat.entities;
 
+import java.io.Serializable;
+
 /**
  * 
  * @author iheng
  *
  */
-public class WeChatMessage {
+
+/**
+ * 消息类 
+ * 定义了客户端与服务器的消息实体
+ * @author iheng
+ *
+ */
+public class WeChatMessage implements Serializable{
+	/*时间戳*/
 	private String timestamp;
-	private String usr_name;
-	private String msg_content;
+	/*发送方*/
+	private String fromUser;
+	/*接收方*/
+	private String toUser;
+	/*消息内容*/
+	private String msgContent;
+	/*消息类型*/
+	private int msgType;
 	
 	public WeChatMessage(){
 		//empty constructor
 	}
 	
-	public WeChatMessage(String timestamp,String usr_name,String msg_content){
+	public WeChatMessage(String timestamp,String fromUser,String msgContent){
 		this.timestamp=timestamp;
-		this.usr_name=usr_name;
-		this.msg_content=msg_content;
+		this.fromUser=fromUser;
+		this.msgContent=msgContent;
 	}
-
-	//getters and setter
-	public String getUsr_name() {
-		return usr_name;
+	public WeChatMessage(String timestamp,String fromUser,String toUser,String msgContent,int msgType){
+		this.timestamp=timestamp;
+		this.fromUser=fromUser;
+		this.toUser=toUser;
+		this.msgContent=msgContent;
+		this.msgType=msgType;
 	}
-
-	public void setUsr_name(String usr_name) {
-		this.usr_name = usr_name;
-	}
-
-	public String getMsg_content() {
-		return msg_content;
-	}
-
-	public void setMsg_content(String msg_content) {
-		this.msg_content = msg_content;
-	}
-
+	
+	/*getters and setters*/
 	public String getTimestamp() {
 		return timestamp;
 	}
@@ -44,5 +51,36 @@ public class WeChatMessage {
 	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
 	}
-	
+
+	public String getFormUser() {
+		return fromUser;
+	}
+
+	public void setFormUser(String formUser) {
+		this.fromUser = formUser;
+	}
+
+	public String getToUser() {
+		return toUser;
+	}
+
+	public void setToUser(String toUser) {
+		this.toUser = toUser;
+	}
+
+	public String getMsgContent() {
+		return msgContent;
+	}
+
+	public void setMsgContent(String msgContent) {
+		this.msgContent = msgContent;
+	}
+
+	public int getMsgType() {
+		return msgType;
+	}
+
+	public void setMsgType(int msgType) {
+		this.msgType = msgType;
+	}
 }
